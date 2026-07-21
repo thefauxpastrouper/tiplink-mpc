4d9526f987f6e1a925d9c749440592140486e3c5
thefauxpastrouper <work.adityaswaroop@gmail.com>
2026-07-21 23:35:47 +0530

    Fix frontend build configuration for Vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import wasm from 'vite-plugin-wasm';
-import topLevelAwait from 'vite-plugin-top-level-await';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    wasm(),
-    topLevelAwait()
+    wasm()
   ],
+  build: {
+    target: 'esnext'
+  },
   server: {
     proxy: {
       '/api': {
